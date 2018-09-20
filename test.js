8af39842a97ae55b8fbe4dca390839a006f8c775
Diego Casas <diacasasce@gmail.com>
2018-09-20 10:45:22 -0500

    evita que se sobre escriba y reinicia el juego

--- test.js
+++ test.js
@@ -21,41 +21,46 @@ function data(pos) {
 }
 function move(mss) {
     let ms = JSON.parse(mss)
-    a[ms.posx][ms.posy] = ms.fig
-    check(ms).then(still=>{
-        if (still){
+    if (a[ms.posx][ms.posy] == ' ') { a[ms.posx][ms.posy] = ms.fig }else{console.log('posicion invalida')}
+    check(ms).then(still => {
+        if (still) {
             console.log()
-    console.log('[' + a[0][0] + '][' + a[0][1] + '][' + a[0][2] + ']')
-    console.log('[' + a[1][0] + '][' + a[1][1] + '][' + a[1][2] + ']')
-    console.log('[' + a[2][0] + '][' + a[2][1] + '][' + a[2][2] + ']')
-        }else{
-            
-            console.log(ms.fig+' Ha ganado')
+            console.log('[' + a[0][0] + '][' + a[0][1] + '][' + a[0][2] + ']')
+            console.log('[' + a[1][0] + '][' + a[1][1] + '][' + a[1][2] + ']')
+            console.log('[' + a[2][0] + '][' + a[2][1] + '][' + a[2][2] + ']')
+        } else {
+            console.log(ms.fig + ' Ha ganado')
+            console.log('volver a jugar')
+            let a = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+            console.log()
+            console.log('[' + a[0][0] + '][' + a[0][1] + '][' + a[0][2] + ']')
+            console.log('[' + a[1][0] + '][' + a[1][1] + '][' + a[1][2] + ']')
+            console.log('[' + a[2][0] + '][' + a[2][1] + '][' + a[2][2] + ']')
         }
     })
-    
+
 }
-function check(ms){
-    let x=ms.posx
-    let y=ms.posy
-    let f=ms.fig
-    return new Promise((resolve,reject)=>{
-            if ((a[x][0]==f)&&(a[x][1]==f)&&(a[x][2]==f)){
-                console.log( 'ganaron')
-                resolve(false)
-            }else if ((a[0][y]==f)&&(a[1][y]==f)&&(a[2][y]==f)){
-                console.log( 'ganaron')
-                resolve(false)
-            }else if((a[0][0]==f)&&(a[1][1]==f)&&(a[2][2]==f)){
-                console.log( 'ganaron')
-                resolve(false)
-            }else if((a[0][2]==f)&&(a[1][1]==f)&&(a[2][0]==f)){
-                console.log( 'ganaron')
-                resolve(false)
-            }else{
-                console.log( 'continua')
-                resolve(true)
-            }
+function check(ms) {
+    let x = ms.posx
+    let y = ms.posy
+    let f = ms.fig
+    return new Promise((resolve, reject) => {
+        if ((a[x][0] == f) && (a[x][1] == f) && (a[x][2] == f)) {
+            console.log('ganaron')
+            resolve(false)
+        } else if ((a[0][y] == f) && (a[1][y] == f) && (a[2][y] == f)) {
+            console.log('ganaron')
+            resolve(false)
+        } else if ((a[0][0] == f) && (a[1][1] == f) && (a[2][2] == f)) {
+            console.log('ganaron')
+            resolve(false)
+        } else if ((a[0][2] == f) && (a[1][1] == f) && (a[2][0] == f)) {
+            console.log('ganaron')
+            resolve(false)
+        } else {
+            console.log('continua')
+            resolve(true)
+        }
     })
 
 }
